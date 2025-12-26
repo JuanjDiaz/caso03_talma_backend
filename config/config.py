1b55c983c370683a08130f9456dce477b8d252e5
juancho060805@gmail.com <juancho060805@gmail.com>
2025-12-25 21:58:17 -0500

    Modificación de host SMTP(ahora Gmail SMTP), implementación de SMTP en los servicios de login para el envío del código

--- config/config.py
+++ config/config.py
@@ -19,10 +19,10 @@ class Settings(BaseSettings):
 
     # SMTP Configuration
     # SMTP Configuration (Renamed to avoid .env conflict)
-    MAIL_HOST: str = "smtp.mailersend.net"
-    MAIL_PORT: int = 2525
-    MAIL_USER: str = "MS_8CwHfS@test-zxk54v8nw61ljy6v.mlsender.net"
-    MAIL_PASSWORD: str = "mssp.0ZklBbf.3vz9dledrx64kj50.psuBIBS"
+    MAIL_HOST: str = "smtp.gmail.com"
+    MAIL_PORT: int = 587
+    MAIL_USER: str = "pruebastalmativit@gmail.com"
+    MAIL_PASSWORD: str = "rztrxmswypvqhxly"
 
     model_config = SettingsConfigDict(
         env_file=".env",
